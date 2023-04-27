c58779dcacca1cd11151a6134d9b0b70607a8176
MaferOrdonez1 <90990182+MaferOrdonez1@users.noreply.github.com>
2023-04-26 20:31:02 -0600

    Add files via upload

---
+++ language.routes.js
@@ -0,0 +1,8 @@
+import {Router, request, response} from "express";
+import { methods as languageController } from "./../controllers/language.controller";
+
+const router = Router();
+
+router.get("/", languageController.getLanguages);
+
+export default router;
